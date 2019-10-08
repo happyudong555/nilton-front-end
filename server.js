cf7efc972563c367ed11b3bfeb8e2cb5b1140532
panjamapon karnasuta <panjamaponkarnasuta@panjamapons-MacBook-Pro.local>
2019-10-08 22:57:31 +0700

    fix production front-end

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const dev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 80;
+const cors = require('cors');
 //const app = next({ dev });
 const app = next({ dir: '.' , dev: false, staticMarkup: false, quiet: false, conf: null, chunk:null, cache: true});
 const handle = app.getRequestHandler()
@@ -10,6 +11,13 @@ const compression = require('compression');
 app.prepare().then(() => {
     const server = express();
     server.use(compression());
+    server.use(cors({ origin: true }));
+    server.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
+        next();
+    });
     // running server
     server.get('*', (req, res) => {
         return handle(req, res)
